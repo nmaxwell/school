b48b2f370c95d37c5a677f5fc1f0af5773a85225
nicholas maxwell <nick@nick-portable.localdomain>
2010-04-04 03:02:10 -0500

    -

--- M97,STOCH_PROC/trans mat/discrete_markov.py
+++ M97,STOCH_PROC/trans mat/discrete_markov.py
@@ -3,6 +3,7 @@ import numpy
 from numpy import *
 import random
 
+
 def random_variable( distribution, values ):
     w = random.random()*sum(distribution)
     s = 0
@@ -15,21 +16,19 @@ def random_variable( distribution, values ):
     return values[-1]
 
 
-
 def sample_path( X0, len_path, P ):
     
-    X = arange(len_path)*0
+    X = zeros(len_path, int )
     N = range(len(P[0]))
     
     X[0] = X0
     for k in range(1,len_path):
-        X[k] = random_variable( P[X[k-1]], N )
+        X[k] = random_variable( P[:,X[k-1]], N )
     
     return X
 
 
-
-def transition_matrix( sample_paths, n_states ):
+def transition_matrix( sample_paths, n_states, state_map= lambda x: x ):
     
     n_paths = len(sample_paths)
     len_path = len(sample_paths[0])
@@ -37,36 +36,33 @@ def transition_matrix( sample_paths, n_states ):
     p = zeros(( len_path, n_states, n_states ))
     
     for X in sample_paths:
-        
         for k in range(1,len_path):
-            j = X[k]
-            
             for l in range(0,k):
-                i = X[l]
-                
-                p[k-l][i][j] += 1
+                p[k-l][ state_map(X[k]) ][ state_map(X[l]) ] += 1
     
     for k in range(len_path):
         for i in range(n_states):
-            s = sum(p[k][i])
+            s = sum(p[k][:,i])
             if s>1E-14:
-                p[k][i] /= sum(p[k][i])
-    
+                p[k][:,i] /= s
+        
     return p
 
 
 
 
+
 def mat_power(A, n):
-    if n==0:
-        return eye(len(A))
-    if n==1:
-        return A
     
-    B = A
-    for k in range(n):
-        B = dot(A,B)
-    return B
+    def mat_power_recurr(B, n ):
+        if n==0:
+            return eye(len(B))
+        if n==1:
+            return B
+        
+        return mat_power_recurr( dot(A,B), n-1)
+    
+    return mat_power_recurr( A, n)
 
 
 
@@ -74,44 +70,32 @@ def mat_power(A, n):
 
 if __name__ == "__main__":
     
-
+    
+    Pij = transpose( array([[0.01,0.01,0.98],[0.8,0.1,0.1],[0.05,.9,0.05]]) )
     
     
-    Pij = array([[0.01,0.01,0.98],[0.8,0.1,0.1],[0.05,.9,0.05]])
-    #Pij = array([[1.,0.,0.],[0.,1.,0.],[0.,0.,1.]])
-    
-    X0 = 2
+    X0 = 1
     v = zeros(3)
     v[X0] = 1
     
     n_paths = 5000
-    len_path = 5
+    len_path = 20
     
     print v
     
-    X = [ sample_path( 2, len_path, Pij)  for n in range(n_paths) ]
+    X = [ sample_path( 1, len_path, Pij)  for n in range(n_paths) ]
     
     p = transition_matrix( X, 3  )
     
-    print Pij, '\n'
+    print Pij, '\n','\n'
     
-    for k in range(1,len(p)-1):
-        
-        #p[k] = numpy.round(p[k],2)
-        print k, dot(p[k],v), '\n', dot(mat_power(Pij, k ),v),'\n\n\n'
+    for k in range(1,5):
+        print around(p[k],2), '\n'
     
+    print '\n','\n'
     
-    
-    #   , '\n', [ sum(p[k][i]) for i in range(len(p[k])) ]
-    
+    for k in range(1,5):
+        print around( mat_power(Pij, k ) ,2),'\n'
     
     
-    """
-    import pylab as p
     
-    for k in range(3):
-        p.plot( X[k] )
-    
-    p.show()
-    """
-
